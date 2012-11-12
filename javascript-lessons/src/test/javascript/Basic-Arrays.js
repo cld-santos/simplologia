85f4991e200e241b394a4fd853a0a1f08cc6b5d9
Claudio Santos <cld.lealsantos@gmail.com>
2012-11-12 13:10:14 -0200

    Adicao de novo teste para Objetos

--- javascript-lessons/src/test/javascript/Basic-Arrays.js
+++ javascript-lessons/src/test/javascript/Basic-Arrays.js
@@ -1,4 +1,18 @@
 describe("Conceitos Básicos de Arrays", function (){
+
+	function Objetao(){
+		
+		var _cor="";
+		
+		this.ObterCor = function(){
+			return _cor;
+		}
+		
+		this.SetarCor = function(value){
+			_cor = value;
+		} 
+	}
+	
 	var lista = [1,2,3,4,5,6,7,8,9];
 
 	it("deve armazenar uma lista de valores crescente.", function(){
@@ -28,6 +42,23 @@ describe("Conceitos Básicos de Arrays", function (){
 		listaDesordenada = [9,4,5,6,2,3,8,1,7];
 		listaDesordenada.sort();
 		expect(listaDesordenada.toString()).toEqual(lista.toString());		
+	});
+
+	it("deve criar uma lista de Objetos", function(){
+		var colecao = new Array();
+		for ( var int = 0; int < 10; int++) {
+			var objeto = new Objetao();
+			objeto.SetarCor("Amarelo "+int);
+			colecao.push(objeto);
+		}
+		expect(colecao.length).toEqual(10);
+		for ( var int = 0; int < 10; int++) {
+			objeto.SetarCor("Amarelo "+int);
+			expect(colecao[int].ObterCor()).toEqual("Amarelo "+int);
+			
+		}
+		
+		
 	});
 	
 });
