8c1c532b8563d8da882892b127b545d2cc7b8475
Claudio Santos <cld.lealsantos@gmail.com>
2013-02-02 19:15:06 -0200

    Novo Prototipo Geotur

--- javascript-lessons/src/main/webapp/js/Main.js
+++ javascript-lessons/src/main/webapp/js/Main.js
@@ -2,5 +2,5 @@
 utils.carregarTemplate(['ProdutosItemView','ProdutosView'], function() {
 	window.appRoteador = new Roteador();
 	Backbone.history.start({pushState: true, root: "/javascript-lessons/"});
-})
+});
 
