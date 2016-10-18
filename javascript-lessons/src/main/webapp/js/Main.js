7f1cf67fd60b02ecef9b6f5a53be22abbc06222b
Claudio Santos <contato@simplologia.com.br>
2016-10-17 22:04:29 -0200

    desmembramento do repositório

--- javascript-lessons/src/main/webapp/js/Main.js
+++
@@ -1,6 +0,0 @@
-
-utils.carregarTemplate(['ProdutosItemView','ProdutosView'], function() {
-	window.appRoteador = new Roteador();
-	Backbone.history.start();//{pushState: true, root: "/javascript-lessons/"});
-});
-
